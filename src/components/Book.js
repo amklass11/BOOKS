fb457c4cda0e0fa5d22306ef25687196b42ffd77
Amk Lass <lassanaa14@gmail.com>
2022-08-14 05:18:15 +0100

    fix errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -23,7 +23,7 @@ const Book = (props) => {
           <button className="blue-font" type="button">Comments</button>
           <span> | </span>
           <button id={id} className="remove blue-font" type="button" onClick={removeHandler}>
-            Remove
+            Remov
           </button>
           <span> | </span>
           <button className="blue-font" type="button">Edit</button>
